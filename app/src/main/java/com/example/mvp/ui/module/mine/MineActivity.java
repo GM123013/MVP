2e81d2a61122651adac5241ed4b486f23855e674
GM123013 <1207585360@qq.com>
2018-08-14 15:24:58 +0800

    我的模块

---
+++ app/src/main/java/com/example/mvp/ui/module/mine/MineActivity.java
@@ -0,0 +1,18 @@
+package com.example.mvp.ui.module.mine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mvp.R;
+
+/**
+ * 我的页面
+ */
+public class MineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
